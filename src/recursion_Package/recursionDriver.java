b5ef4478a08d3e034a97c0b9b6079784caae5da1
Cristy Lopez <cristy025@gmail.com>
2020-04-17 00:24:00 -0700

    Updated driver
    Removed unnecessary comments.

--- src/recursion_Package/recursionDriver.java
+++ src/recursion_Package/recursionDriver.java
@@ -8,23 +8,5 @@ public class recursionDriver {
     public static void main(String args[]) throws InterruptedException{
     recursion NewRec = new recursion();	//creates new instance of the recursion class
     NewRec.rTest();						//goes to the method rTest in the newly named class
-    
-    	//rTest(0); //does not work
-//    	newT();
-    } //end main 
+    }
 } //end class
-
-//excerpt 1:
-
-//protected static int rTest(int a){
-//rTest();
-//
-//a = a + 1;
-//
-//System.out.println(a);
-//if(a <= 10){
-//	 return a = 2;
-//}
-//	return rTest(a);
-//
-//}
